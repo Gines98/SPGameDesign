543742fe3e2abd7628ddf6be95aea8f96096864f
sameneses <sameneses@uc.cl>
2023-11-28 11:32:16 -0300

    mejoras a la pistola

--- Assets/Scripts/Disparos/Pistola.cs
+++ Assets/Scripts/Disparos/Pistola.cs
@@ -7,6 +7,7 @@ public class Pistola : MonoBehaviour
     [SerializeField] float shootOffset;
     [SerializeField] int power;
     [SerializeField] float speed;
+    [SerializeField] Sprite bala;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +22,15 @@ public class Pistola : MonoBehaviour
         this.transform.rotation = Quaternion.AngleAxis(angulo, Vector3.forward);
         if (Input.GetMouseButtonUp(0))
         {
-            BulletManager.Inst.Disparar(1, this.transform.position + this.transform.rotation * (Vector3.right * shootOffset), speed, this.transform.rotation, "Player");
+            BulletManager.Inst.Disparar(power, this.transform.position + this.transform.rotation * (Vector3.right * shootOffset), speed, this.transform.rotation, "Player", bala);
         }
+    }
+
+    public void ChangeGun(float offset, int bulletPower, float bulletSpeed, Sprite bullet)
+    {
+        shootOffset = offset;
+        power = bulletPower;
+        speed = bulletSpeed;
+        bala = bullet;
     }
 }
