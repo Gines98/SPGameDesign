97a6d4f1768091428d69b08843d4a71f8c4e1b93
Simon Carreño <scarreno@dark-ware.com>
2023-12-15 01:13:50 -0300

    Modified the "Bull" as a new Melee enemy

--- Assets/Prefabs/Bull/BullScript.cs
+++ Assets/Prefabs/Bull/BullScript.cs
@@ -1,26 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-public class BullScript : MonoBehaviour
+public class BullScript : EnemyComponent
 {
-    [Tooltip("Configuration Scriptable Object")]
-    public EnemyConfigObject configObject;
+    protected void Start()
+    {
+        base.Start();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Player")
         {
             if (!collision.collider.GetComponent<HuasoScript>().beignHurt)
             {
-                collision.collider.GetComponent<Rigidbody2D>().AddForce(new Vector2(collision.collider.GetComponent<HuasoScript>().speed * -configObject.thrust, collision.collider.GetComponent<HuasoScript>().speed * 50));
-                collision.collider.GetComponent<HuasoScript>().health -= configObject.damage;
+                collision.collider.GetComponent<Rigidbody2D>().AddForce(new Vector2(collision.collider.GetComponent<HuasoScript>().speed * -configParameters.thrust, collision.collider.GetComponent<HuasoScript>().speed * 50));
+                collision.collider.GetComponent<HuasoScript>().health -= configParameters.damage;
                 if (collision.collider.GetComponent<HuasoScript>().health <= 0)
                 {
                     collision.collider.GetComponent<HuasoScript>().health = 0;
                     collision.collider.GetComponent<HuasoScript>().alive = false;
                     RunHuasoRun.instance.LevelEnd(false);
                 }
-                if (configObject.destructible)
+                if (configParameters.destructible)
                 {
                     Destroy(gameObject);
                 }
@@ -35,25 +39,15 @@ public class BullScript : MonoBehaviour
 
     private void Awake()
     {
-        if(configObject.spawnSound)
-        GetComponent<AudioSource>().PlayOneShot(configObject.spawnSound);
+        if(configParameters.spawnSound)
+        GetComponent<AudioSource>().PlayOneShot(configParameters.spawnSound);
     }
 
     private void Update()
     {
-        if(RunHuasoRun.instance.endlessLevel)
-        {
-            float difficulty = Time.timeSinceLevelLoad / 30;
-            if (difficulty < 1) difficulty = 1;
-            if (difficulty > 5) difficulty = 5;
-            transform.position -= transform.right * configObject.speed * Time.deltaTime * (difficulty / 2);
-        }
-        else
-        {
-            transform.position -= transform.right * configObject.speed * Time.deltaTime;
-        }
-        
-        
+
+        transform.position -= transform.right * configParameters.speed * Time.deltaTime;
+            
     }
 
 }
